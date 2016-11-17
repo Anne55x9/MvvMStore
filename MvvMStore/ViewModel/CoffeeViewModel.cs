bc600c5cfca01be570b2318483cf50884a8dfa87
anne55x9 <anne55x9@edu.easj.dk>
2016-11-17 13:47:06 +0100

    AddbuttonMade_MvvMStore

--- MvvMStore/ViewModel/CoffeeViewModel.cs
+++ MvvMStore/ViewModel/CoffeeViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using MvvMStore.Model;
+using MvvMStore.ViewModel;
 
 namespace MvvMStore
 {
@@ -26,13 +28,36 @@ namespace MvvMStore
             set { selectedCoffee = value; OnPropertyChanged(nameof(SelectedCoffee));}
         }
 
+        private Model.Coffee insertCoffee;
+
+        public Model.Coffee InsertCoffee
+        {
+            get { return insertCoffee; }
+            set { insertCoffee = value; }
+        }
+
+        private RelayCommand addCoffeeCommand;
+
+        public RelayCommand AddCoffeeCommand
+        {
+            get { return addCoffeeCommand; }
+            set { addCoffeeCommand = value; }
+        }
+
+        public void AddNewCoffee()
+        {
+            coffeeList.Add(insertCoffee);
+        }
+
 
         public CoffeeViewModel()
         {
-            CoffeeList = new Model.CoffeeList();
+            coffeeList = new Model.CoffeeList();
             selectedCoffee = new Model.Coffee();
+            insertCoffee = new Model.Coffee();
+            addCoffeeCommand = new RelayCommand(AddNewCoffee);
+           
         }
-
 
         public event PropertyChangedEventHandler PropertyChanged;
 
---
+++ MvvMStore/ViewModel/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace MvvMStore.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private Action methodToExecute = null;
+        private Func<bool> methodTODetectCanExecute = null;
+        private Action addNewCoffee;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if(this.methodTODetectCanExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                return this.methodTODetectCanExecute();
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.addNewCoffee();
+        }
+
+
+        public RelayCommand(Action addNewCoffee)
+        {
+            this.addNewCoffee = addNewCoffee;
+        }
+
+        
+    }
+}
